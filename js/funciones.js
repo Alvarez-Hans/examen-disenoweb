8b18146bf60107389c90c0a0cff06342c4ae9f3c
Hans Alvarez <60480071+Alvarez-Hans@users.noreply.github.com>
2022-01-21 01:10:54 -0300

    entrega de los fuentes

---
+++ js/funciones.js
@@ -0,0 +1,87 @@
+function fntInicio()
+{
+  console.log('fntIncio');
+  var html = `<h3 id="subTitulo">ANIMALES DE COMPAÑIA</h3>
+  <div>
+    <div id="tableFotosAnimal">
+      <div id="descripcionAnimal">
+        <p >
+          Un animal de compañía, mascota o animal doméstico, es un animal domesticado que se conserva con el propósito de brindar compañía o para el disfrute del cuidador. A diferencia de los animales de laboratorio, animales para la crianza o ganado, animales para el transporte o animales para el deporte, los animales de compañía no son conservados para traer beneficios económicos o alimenticios, aunque sí un beneficio personal.
+          Los animales de compañía son seleccionados por su comportamiento, adaptabilidad y por su interacción con los humanos, en la que posiblemente se utilicen como herramientas de caza o seguridad. Poseer un animal de compañía puede traer beneficios a la salud, ya que el cuidado diario hace olvidar a muchas personas otro tipo de preocupaciones, evita la depresión y nos hace sentir útiles. Los animales de compañía más populares son el perro y el gato.
+          El animal de compañía también se caracteriza por ser amigable. Se puede decir que la mayoría de estos animales se convierten en domésticos si la persona con la que conviven les enseña buenos modales en la convivencia diaria, no olvidando su naturaleza animal.
+          Un animal de compañía se adapta al ambiente de un ser humano sin importar su naturaleza: se adhiere a un grupo, familia o sociedad, la cual le dota de todo lo necesario para su desarrollo.
+          En adaptabilidad y comportamiento, se dice que los perros como animales de compañía, superan a la mayoría. Su adaptabilidad ha ayudado al hombre en diversos ámbitos sociales, culturales y recreativos, como en su uso terapéutico en hospitales, como guías de personas ciegas, como apoyo policial, y en complicados rescates de personas.
+          La compañía de cualquier mascota evoca siempre ternura, compañía y amistad, cualidades que se intensifican cuando la soledad busca filtrarse en la vida de las personas; es en estos momentos cuando la mascota adquiere un papel más importante, al sentirlo parte de la familia y aún más, como un amigo.
+          La mayoría de las personas muestran una actitud positiva a través de su relación con una mascota o animal de compañía, sobre todo aquellos que sufren problemas de tipo psicológico o físico.
+        </p>
+      </div>
+      <div id="fotoAnimal"><img src="img/animales1.jpg" alt=""></div>
+    </div>
+  </div>
+  `;
+  document.getElementById("detalle").innerHTML = html;
+}
+
+function fntGatos()
+{
+  console.log('fntGatos');
+  var html = `<h3 id="subTitulo">GATOS</h3>
+  <p id="descripcion">El gato doméstico​ (Felis silvestris catus), llamado más comúnmente gato, y de forma coloquial minino, michino, michi, micho, mizo, miz, morroño o morrongo, y algunos nombres más, es un mamífero carnívoro de la familia Felidae. Es una subespecie domesticada por la convivencia con el ser humano.
+      El nombre actual en muchas lenguas proviene del latín vulgar catus. Irónicamente, catus aludía a los gatos salvajes, mientras que los gatos domésticos, en latín, eran llamados felis.
+      Como resultado de mutaciones genéticas, cruzamiento y selección artificial, hay numerosas razas. Algunas, como la raza sphynx o la peterbald están desprovistas de pelo; otras carecen de cola, como los gatos de la raza manx, y algunas tienen coloraciones atípicas, como los llamados gatos azules.
+      El gato se comunica a través de vocalizaciones. Las más populares son su característico maullido y el ronroneo, pero puede aullar, gemir, gruñir y bufar.​Los Gatos desarrollaron el maullido con la única finalidad de poder comunicarse con el ser humano. Además, adopta poses o expresiones que informan, a sus congéneres, sus enemigos o sus cuidadores, de su ánimo o sus intenciones.
+      Junto con el perro, es el animal doméstico más popular, como mascota, como ayuda en la lucha contra roedores o ambas cosas.
+      Por su amplio abanico de presas potenciales, por su alta eficiencia como depredador, y por su elevado éxito reproductivo —especialmente si se suministra artificialmente alimento a las colonias sin tomar medidas adicionales para limitar su fertilidad— el gato doméstico está incluido en la lista de las diez especies exóticas invasoras más dañinas del mundo de la Unión Internacional para la Conservación de la Naturaleza.</p><div>
+      <div>
+      <div id="tableFotosVertical">
+          <div id="foto1"><img src="img/gato1.jpg" alt=""></div>
+          <div id="foto2"><img src="img/gato2.jpg" alt=""></div>
+        </tr>
+      </div>
+    </div>
+  `;
+  document.getElementById("detalle").innerHTML = html;
+}
+
+function fntPerros()
+{
+  console.log('fntPerros');
+  var html = `<h3 id="subTitulo">PERROS</h3>
+  <p id="descripcion">
+    El perro (Canis familiaris o Canis lupus familiaris, dependiendo de si se lo considera una especie por derecho propio o una subespecie del lobo), llamado perro doméstico o can, y en algunos lugares coloquialmente llamado chucho, tuso, choco, entre otros; es un mamífero carnívoro de la familia de los cánidos, que constituye una especie del género Canis. En el 2013, la población mundial estimada de perros estaba entre setecientos millones y novecientos ochenta y siete millones. Su tamaño (o talla), su forma y su pelaje es muy diverso y varía según la raza. Posee un oído y un olfato muy desarrollados, y este último es su principal órgano sensorial. Su longevidad media es de diez a trece años, dependiendo de la raza.
+    El perro doméstico proviene de un grupo ancestral común que data de hace aproximadamente treinta mil años, y desde entonces se ha extendido a todas partes del mundo. Los primeros restos fósiles de perros enterrados junto con seres humanos se encontraron en Israel, y datan de hace unos doce mil años. Desde entonces, los perros y los humanos han evolucionado conjuntamente, tanto en las culturas africanas y euroasiáticas, como en las que poblaron América y se mantuvieron sin contacto con aquellas hasta el siglo XV. Los perros comparten el entorno, los hábitos y el estilo de vida humanos, como las dietas ricas en cereales y almidón. La alimentación inadecuada, así como el uso de antibióticos, son la causa del desarrollo de muchas enfermedades inflamatorias e inmunológicas. Unas cuatrocientas enfermedades del perro tienen una equivalente humana, y destacan especialmente la enfermedad de Alzheimer y otros trastornos neurológicos, así como varios tipos de cáncer, enfermedades autoinmunes y enfermedades cardiovasculares.
+    Tienen una gran relación con los humanos, entre tales relaciones se incluyen servir como animales de compañía, animales de guardia, perros de trabajo, perros de caza, galgos de carrera, perros guía, perros pastores o perros boyeros.</p>
+    <div>
+      <div id="tableFotosVertical">
+          <div id="foto1"><img src="img/perro1.jpg" alt=""></div>
+          <div id="foto2"><img src="img/perro2.jpg" alt=""></div>
+        </tr>
+      </div>
+    </div>
+    `;
+  document.getElementById("detalle").innerHTML = html;
+}
+
+function fntErizos()
+{
+  console.log('fntErizos');
+  var html = `<h3 id="subTitulo">ERIZOS</h3>
+  <p id="descripcion">
+    Los erizos (Erinaceinae) son pequeños mamíferos cubiertos de púas; pertenecen al orden Eulipotyphla, aunque antiguamente eran considerados del orden Insectívoro, hoy en día obsoleto. Hay un total de dieciséis especies de erizos divididas en cinco géneros; el único erizo que no pertenece a esos géneros es el erizo gigante o también conocido como Erethizontidae.
+    Los erizos habitan Europa, Asia, África, y han sido introducidos en Nueva Zelanda. No hay erizos endémicos de Oceanía, ni tampoco erizos vivos que sean nativos de Norteamérica. En España viven dos especies, el erizo europeo y el erizo moruno, pero en cautividad es muy frecuente el erizo orejudo. Los erizos no han cambiado mucho durante los últimos quince millones de años. Como muchos de los primeros mamíferos, han adoptado una vida principalmente nocturna y una alimentación insectívora.
+  </p>
+  <div>
+    <div id="tableFotosVertical">
+        <div id="foto1"><img src="img/erizo1.jpg" alt=""></div>
+        <div id="foto2"><img src="img/erizo2.jpg" alt=""></div>
+      </tr>
+    </div>
+  </div>
+  
+    `;
+  document.getElementById("detalle").innerHTML = html;
+}
+
+function btnEnviar() {
+  alert(`${TxtNombre.value} tu mensaje ha sido enviado`)
+}
